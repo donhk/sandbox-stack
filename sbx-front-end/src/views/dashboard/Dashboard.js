71c473394a36e651c10bc070bd2fc79588f49b66
Frederick Alvarez <hk.fred.0@gmail.com>
2025-04-06 22:59:16 -0700

    fix resources

--- sbx-front-end/src/views/dashboard/Dashboard.js
+++ sbx-front-end/src/views/dashboard/Dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import {
     CCard,
-    CCardBody,
-    CHeader,
+    CCardBody, CCol,
+    CHeader, CRow,
 } from '@coreui/react'
 import {ResourcesChart} from "src/views/dashboard/ResourcesCharts";
 import {VMResourcesChart} from "src/views/dashboard/VMResourcesChart";
@@ -11,28 +11,34 @@ const Dashboard = () => {
 
     return (
         <>
-            <CCard className="mb-4">
-                <CHeader>
-                    <h4 id="resources" className="card-title mb-0">
-                        Resource Utilization
-                    </h4>
-                    <div className="small text-body-secondary">January - March 2025</div>
-                </CHeader>
-                <CCardBody>
-                    <ResourcesChart/>
-                </CCardBody>
-            </CCard>
-            <CCard className="mb-4">
-                <CHeader>
-                    <h4 id="resources" className="card-title mb-0">
-                        VMs History
-                    </h4>
-                    <div className="small text-body-secondary">January - March 2025</div>
-                </CHeader>
-                <CCardBody>
-                    <VMResourcesChart/>
-                </CCardBody>
-            </CCard>
+            <CRow>
+                <CCol xs={6}>
+                    <CCard className="mb-4">
+                        <CHeader>
+                            <h4 id="resources" className="card-title mb-0">
+                                Resource Utilization
+                            </h4>
+                            <div className="small text-body-secondary">January - March 2025</div>
+                        </CHeader>
+                        <CCardBody>
+                            <ResourcesChart/>
+                        </CCardBody>
+                    </CCard>
+                </CCol>
+                <CCol xs={6}>
+                    <CCard className="mb-4">
+                        <CHeader>
+                            <h4 id="resources" className="card-title mb-0">
+                                VMs History
+                            </h4>
+                            <div className="small text-body-secondary">January - March 2025</div>
+                        </CHeader>
+                        <CCardBody>
+                            <VMResourcesChart/>
+                        </CCardBody>
+                    </CCard>
+                </CCol>
+            </CRow>
         </>
     )
 }
